4c505cc4ebc42d6fedc94309ba70c9fdda43e4ff
Z <Z@gmail.com>
2025-05-03 19:29:34 -0400

    fixed function calculateArea() and added function calculateTotal()

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,6 +5,17 @@ function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
     let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotal() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById('result2').innerText = `The total amount is: ${total}`;
+}
